516a572d92acb951bc7c37401938ca5317479077
denis_stepanok <elfdenis@gmail.com>
2025-06-12 22:43:44 +0300

    finished module 3

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -24,43 +27,42 @@ export const setupServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
-      message: `Mongo connection successfully established! Use endpoint '/contacts' or '/contacts/:contactId'`,
+      message: 'Mongo connection successfully established!',
+      availableEndpoints: {
+        GET: [
+          { path: '/contacts', description: 'Get all contacts' },
+          { path: '/contacts/:contactId', description: 'Get contact by ID' },
+        ],
+        POST: [
+          {
+            path: '/contacts',
+            description: 'Create new contact (name, email, phone)',
+          },
+        ],
+        PUT: [
+          {
+            path: '/contacts/:contactId',
+            description: 'Replace entire contact by ID',
+          },
+        ],
+        PATCH: [
+          {
+            path: '/contacts/:contactId',
+            description: 'Partially update contact by ID',
+          },
+        ],
+        DELETE: [
+          { path: '/contacts/:contactId', description: 'Delete contact by ID' },
+        ],
+      },
     });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use(contactsRouter);
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
