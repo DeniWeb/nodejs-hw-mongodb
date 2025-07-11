929a1cfd12202add22033725ec36b28a331e86e4
denis_stepanok <elfdenis@gmail.com>
2025-07-11 12:04:55 +0300

    added photo upload when creating a contact

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -55,7 +55,20 @@ export const getContactByIdController = async (req, res) => {
 
 export const createContactController = async (req, res) => {
   const userId = req.user._id;
-  const contact = await createContact({ ...req.body, userId });
+
+  const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar(CLOUDINARY.ENABLE_CLOUDINARY).toLowerCase() === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({ ...req.body, userId, photo: photoUrl });
 
   res.status(201).json({
     status: 201,
