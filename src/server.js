9bfeed8999f37f5411a6a3690c9a5ca2b48934e4
denis_stepanok <elfdenis@gmail.com>
2025-07-18 00:06:23 +0300

    Add api documentation

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -27,6 +28,7 @@ export const setupServer = () => {
     }),
   );
 
+  app.use('/api-docs', swaggerDocs());
   app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.get('/', (req, res) => {
